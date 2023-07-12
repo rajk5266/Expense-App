b77c8edadc7cd0fc26f389db1aa6c1301b704581
rajk5266 <kushwaharaj903@gmail.com>
2023-07-12 13:16:59 +0530

    complete email integration system sendinBlue

--- controllers/resetPasswordController.js
+++ controllers/resetPasswordController.js
@@ -1,46 +1,111 @@
-// const uuid = require('uuid')
-const SibApiV3Sdk = require("sib-api-v3-sdk"); 
-// const bcrypt = require("bcrypt"); 
-const dotenv = require("dotenv").config(); 
+
+const uuid = require('uuid')
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+const bcrypt = require("bcrypt");
+// require("dotenv").config(); 
 
 const path = require('path')
 const User = require('../models/user')
 const ForgotPassword = require('../models/forgotPassword');
 
-const client = SibApiV3Sdk.ApiClient.instance; 
-const apikey = client.authentications["api-key"]; 
-apikey.apiKey = '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'; 
+const client = SibApiV3Sdk.ApiClient.instance;
+const apikey = client.authentications["api-key"];
+apikey.apiKey = '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';
 
-exports.showResetPasswordForm = (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'resetPassword.html'))
+
+exports.showforgotPasswordForm = (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'forgotPassword.html'))
 }
 
-exports.sendLink = async (req, res) => {
-    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-        const sender = {
-            email: "kushwaharaj903@gmail.com",
-            name: "RAJ KUSHWAHA"
-        };
-        const receivers = [
-            {
-                email: req.body.email
+exports.forgotPassword = async (req, res) => {
+    console.log(req.body)
+    try {
+        const { email } = req.body
+        const user = await User.findOne({
+            where: { email }
+        })
+        if (user) {
+            const id = uuid.v4()
+            console.log(id)
+            ForgotPassword.create({ id, userId: req.user, isactive: true })
+                .catch(err => {
+                    console.log(err)
+                })
+            const sendinblue = new SibApiV3Sdk.TransactionalEmailsApi();
+            const sendSmtpemail = {
+                to: [{ email }],
+                sender: {
+                    email: "kushwaharaj903@gmail.com",
+                    name: "RAJ KUSHWAHA",
+                },
+                subject: "reset your password",
+                htmlContent: `<p>Hello,</p> 
+                       <p>Please click the following link to reset your password:</p> 
+                       <p><a href="http://localhost:8000/password/resetpassword/${id}">Reset password</a></p> 
+                       <p>If you did not request a password reset, please ignore this email.</p> 
+                       <p>Thank you!</p>`,
             }
-        ]
-            try{
-           const sendEmail = await apiInstance.sendTransacEmail({
-            sender,
-            to: receivers,
-            subject: "test email from brevo",
-            textContent: "test email",
-            htmlContent: `<p>Hello,</p> 
-            <p>Please click the following link to reset your password:</p> 
-            <p><a href="http://15.206.28.85:3000/password/resetpassword/">Reset password</a></p> 
-            <p>If you did not request a password reset, please ignore this email.</p> 
-            <p>Thank you!</p>`, 
-    
-           });
-           return res.send(sendEmail) 
-        } catch (err) {
+            await sendinblue.sendTransacEmail(sendSmtpemail);
+            return res.status(200).json({ message: "link has been send to reset password", success: true })
+        }
+        else {
+            console.log('user doesnpt exist')
+            return res.status(404).json('user does not exist')
+        }
+    } catch (err) {
+        console.log(err)
+        return res.json({ message: err, success: false })
+    }
+}
+
+exports.showResetPasswordForm = async (req, res) => {
+    // console.log(req.)
+    try {
+        const id = req.params.id
+        const idExist = await ForgotPassword.findOne({
+            where: {
+                id,
+                isactive: true
+            }
+        })
+     
+        res.sendFile(path.join(__dirname, '..', 'views', 'resetPasswordForm.html'))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+exports.updatePassword = async (req, res) => {
+    try{
+        const{id, confirmPassword} = req.body;
+        const forgotdetails = await ForgotPassword.findOne({
+            where:{
+                id
+            }
+        })
+        forgotdetails.update({
+            isactive: false
+        })
+        const userId = forgotdetails.userId;
+        const user = await User.findOne({
+            where: {
+                id: userId
+            }
+        })
+        if(user){
+            const saltrounds = 10;
+            bcrypt.hash(confirmPassword, saltrounds, async(err, hash) => {
+                await user.update({
+                    password: hash
+                })
+                res.status(200).json({message: "password updated succesfully"})
+                
+            })
+        }
+        else{
+            res.status(404).json({message : 'user does not exist', success: false})
+        }
+    }catch(err){
         console.log(err)
     }
 }
