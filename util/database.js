6f414404055014ad9eaeffe5bc069794b50fd093
rajk5266 <kushwaharaj903@gmail.com>
2023-07-21 01:45:56 +0530

    alls et

--- util/database.js
+++ util/database.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('expense-tracker-app', 'root', 'Hannah903@ophio', {
-    dialect: 'mysql',
-    host: 'localhost'
+console.log('---->')
+const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_ID, process.env.MYSQL_PASSWORD, {
+    dialect: process.env.MYSQL_DIALECT,
+    host: process.env.MYSQL_HOST,
 });
 
 module.exports = sequelize;
