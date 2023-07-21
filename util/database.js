810c0a40aab0671c1e04d09598673540b26aeb3c
rajk5266 <kushwaharaj903@gmail.com>
2023-07-21 11:34:25 +0530

    dialect fixed

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const Sequelize = require('sequelize')
 console.log('---->')
 const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_ID, process.env.MYSQL_PASSWORD, {
-    dialect: process.env.MYSQL_DIALECT,
+    dialect: 'mysql',
     host: process.env.MYSQL_HOST,
 });
 
